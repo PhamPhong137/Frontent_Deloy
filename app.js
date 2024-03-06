cd0e08610c82ff57eb9c52ded0b3f5e0b023700d
PhamPhong137 <pp072003@gmail.com>
2024-03-06 16:41:47 +0700

    deloy frontend

---
+++ app.js
@@ -0,0 +1,13 @@
+// const slides docyment.jery ouerySelectotAll
+const slides= document.querySelectorAll('.slide')
+for (const slide of slides){
+	slide.addEventListener('click',()=>{
+		clearActiveClasses()
+		slide.classList.add('active')
+	})
+}
+function clearActiveClasses(){
+	slides.forEach((slide) =>{
+		slide.classList.remove('active')
+	})
+}
